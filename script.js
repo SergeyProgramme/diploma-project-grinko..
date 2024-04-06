d5ad885c2229bd151d7678e47e1761b512a8710c
Sergey Grinko <sg2813716@gmail.com>
2024-04-06 11:07:00 +0300

    added

---
+++ script.js
@@ -0,0 +1,51 @@
+let names = ['Sergey','Arina','Dima','Vasya']
+let ages = [23,14,25,30]
+let fruits = ['яблуко','апельсин','банан','мандарин']
+console.log(names)
+console.log(ages)
+console.log(fruits)
+
+console.log(names[1])
+console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numberArray = [1,2,3,4,5];
+// console.log(numberArray)
+
+// let numberArray = [1,2,3,4,5];
+// let fruitsArray = ['яблоко', 'банан', 'апельсин']
+// let mixedArray = [1,'два', true];
+
+// numberArray.push(6)
+// numberArray.unshift(0)
+// numberArray.pop()
+// numberArray.shift()
+// console.log(numberArray)
+
+// const task = ['зібрати підручники до школи','виконати домашне завдання']
+// const lastTask = 'погуляти з друзями';
+// task.concat(lastTask)
+// let resultTasks = task.concat(lastTask)
+// console.log(resultTasks)
+
+// const number = [1,3,7,2,9]
+// let user = prompt('Вгадайте число від 1 до 10:')
+// if (number.includes(parseInt(user))){
+//     console.log('Правильно, ви вгадали')
+// } else{
+//     console.log('Ви невгадали, спробуйте ще раз')
+// }
